94c2e6738867458d8507ca6cfc24716018be94c9
Arshia Haeri-Mehrizi <haeri.arshia@gmail.com>
2023-06-27 18:10:15 +0430

    initial API with Flask

---
+++ Todo-List/api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def add_task():
+    pass
+
+def get_tasks():
+    pass
+
+def edit_task():
+    pass
+
+def remove_task():
+    pass
+
+if __name__ == '__main__':
+    app.run()
