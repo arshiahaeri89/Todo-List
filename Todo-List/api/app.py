8b1eff2b7f644347058dfe9219c67df88a010684
Arshia Haeri-Mehrizi <haeri.arshia@gmail.com>
2023-07-13 20:39:56 +0330

    login func and some fix and url changes

--- Todo-List/api/app.py
+++ Todo-List/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import enum
+import datetime
 import config
 
 app = Flask(__name__)
@@ -33,18 +34,24 @@ class Task(db.Model):
         return f'<Task "{self.title}">'
 
 
-@app.route('/add_task', methods=['POST'])
+@app.route('/tasks/add', methods=['POST'])
 def add_task():
-    title = request.form['task_title']
+    title = request.form['task_title'] # TODO: in error handling handle this with key_error
     desc = request.form['task_desc']
     status = request.form['task_status']
     start_date = request.form['task_start_date']
     end_date = request.form['task_end_date']
     user_id = int(request.form['user_id'])
 
-    user = User.query.get(user_id).first()
+    user = User.query.get(user_id)
     if user:
-        task = Task(titile=title, desc=desc, status=status, start_date=start_date, end_date=end_date, user_id=user_id)
+        start_date_datetime = datetime.datetime.strptime(
+            start_date, config.DATETIME_FORMAT)
+        
+        end_date_datetime = datetime.datetime.strptime(
+            end_date, config.DATETIME_FORMAT)
+        
+        task = Task(title=title, desc=desc, status=status, start_date=start_date_datetime, end_date=end_date_datetime, user_id=user_id)
         db.session.add(task)
         db.session.commit()
 
@@ -59,7 +66,7 @@ def add_task():
 
 
 
-@app.route('/get_tasks', methods=['POST'])
+@app.route('/q/tasks/', methods=['POST'])
 def get_tasks():
     username = request.form['username'] # TODO: in error handling handle this with key_error
     user = User.query.filter_by(username=username).first()
@@ -71,7 +78,7 @@ def get_tasks():
             task_dict = {
                 'task_title': task.title,
                 'task_desc': task.desc,
-                'task_status': task.status,
+                'task_status': task.status.name,
                 'task_start_date': task.start_date,
                 'task_end_date': task.end_date
             }
@@ -96,8 +103,9 @@ def edit_task():
 def remove_task():
     pass
 
-@app.route('/register', methods=['POST'])
+@app.route('/account/register', methods=['POST'])
 def register():
+    # TODO: in error handling handle this with key_error
     username = request.form['username'] # TODO: Handle When a User Exists and unique constraint failed 
     password = request.form['password']
     user = User(username=username, password=password)
@@ -107,6 +115,24 @@ def register():
     data = {
         'status': 'ok'
     }
+
+    return jsonify(data)
+
+
+@app.route('/account/login', methods=['POST'])
+def login():
+    # TODO: in error handling handle this with key_error
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter_by(username=username, password=password).first()
+    if user:
+        data = {
+            'status': 'ok'
+        }
+    else:
+        data = {
+            'status': 'not found'
+        }
 
     return jsonify(data)
 
