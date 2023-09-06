95abf9682ad15fc9a1074f759e1510bdc9ace603
Arshia Haeri-Mehrizi <haeri.arshia@gmail.com>
2023-09-06 11:28:43 +0330

    ready to publish APK

--- Todo-List/clients/mobile/Todo_list/src/app/app.component.ts
+++ Todo-List/clients/mobile/Todo_list/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   
   async ngOnInit() {
     await this.storage.create();
-    await this.storage.set("baseURL", "http://127.0.0.1:5000");
+    await this.storage.set("baseURL", "http://192.168.1.105:5000");
   }
   
 }
